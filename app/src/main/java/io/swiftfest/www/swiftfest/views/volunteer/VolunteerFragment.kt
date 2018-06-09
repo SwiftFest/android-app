98c80dffea23d2b07bdac6d032219cdc0f331f09
Chris Buonocore <cbuonocore@drift.com>
2018-06-09 16:58:22 -0400

    update volunteer dialog

--- app/src/main/java/io/swiftfest/www/swiftfest/views/volunteer/VolunteerFragment.kt
+++ app/src/main/java/io/swiftfest/www/swiftfest/views/volunteer/VolunteerFragment.kt
@@ -11,13 +11,18 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.afollestad.materialdialogs.MaterialDialog
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.common.FlexibleItemDecoration
 import io.swiftfest.www.swiftfest.R
 import io.swiftfest.www.swiftfest.data.DataProvider
+import io.swiftfest.www.swiftfest.data.model.Social
 import io.swiftfest.www.swiftfest.data.model.Volunteer
+import io.swiftfest.www.swiftfest.utils.loadUriInCustomTab
+import kotlinx.android.synthetic.main.volunteer_dialog_layout.*
 import kotlinx.android.synthetic.main.volunteer_fragment.*
 
 
@@ -50,20 +55,6 @@ class VolunteerFragment : Fragment(), FlexibleAdapter.OnItemClickListener {
     override fun onItemClick(itemView: View, position: Int): Boolean {
         val item = volunteerAdapter.getItem(position) as VolunteerAdapterItem
         val volunteer = item.itemData
-        val bodyText = StringBuilder()
-        val titleText: String
-
-        if (volunteer.title.isNullOrBlank()) {
-            titleText = "Volunteer"
-        } else {
-            titleText = volunteer.title!!
-        }
-
-        bodyText.append(titleText).append("\n---")
-
-        volunteer.social.map {
-            bodyText.append("\n${it.name.capitalize()}:\n${it.link}\n")
-        }
 
         val context = activity as Context
 
@@ -81,10 +72,44 @@ class VolunteerFragment : Fragment(), FlexibleAdapter.OnItemClickListener {
         val view = dialog.getCustomView()
         if (view != null) {
             view.findViewById<TextView>(R.id.title).text = titleHeaderText
-            view.findViewById<TextView>(R.id.body).text = bodyText.toString()
+            val titleText: String
+            if (volunteer.title.isNullOrBlank()) {
+                titleText = "Volunteer"
+            } else {
+                titleText = volunteer.title!!
+            }
+
+            view.findViewById<TextView>(R.id.subTitle).text = titleText
+//            view.findViewById<TextView>(R.id.body).text = bodyText.toString()
+            val socialLayout = view.findViewById<LinearLayout>(R.id.socialLayout)!!
+
+            volunteer.social.map {
+                addSocialItemToLayout(socialLayout, it)
+            }
+
+
         }
 
         return true
+    }
+
+    private fun addSocialItemToLayout(socialLayout: LinearLayout, it: Social) {
+        val socialImage = ImageView(activity)
+        socialImage.setImageResource(getSocialResource(it.name))
+        socialImage.setPadding(8, 8, 8, 8)
+        socialImage.setOnClickListener { v ->
+            activity?.loadUriInCustomTab(String.format("%s", it.link))
+        }
+        socialLayout.addView(socialImage)
+    }
+
+    private fun getSocialResource(name: String): Int = when (name.toLowerCase()) {
+        "twitter" -> R.drawable.social_twitter
+        "linkedin" -> R.drawable.social_linkedin
+        "facebook" -> R.drawable.social_facebook
+        "instagram" -> R.drawable.social_instagram
+        "github" -> R.drawable.social_github
+        else -> R.drawable.social_site
     }
 
 
